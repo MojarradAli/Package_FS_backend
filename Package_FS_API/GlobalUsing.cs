f239c4a631dcbfab8f228f1ba61b015ebc427506
Abbas Hajihashemi <hajihashemi_a@nicode.org>
2023-01-31 17:46:25 +0330

    Configs

--- Package_FS_API/GlobalUsing.cs
+++ Package_FS_API/GlobalUsing.cs
@@ -1,5 +1,6 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using Package_FS_API.Entities;
+global using Package_FS_API.Data;
 global using Package_FS_API.Services;
 global using Package_FS_API.Services.DTOs;
 
--- Package_FS_API/Program.cs
+++ Package_FS_API/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add Database
+var connectionString = builder.Configuration.GetConnectionString("Default");
+
+builder.Services.AddDbContext<MainDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Add Services
+builder.Services.AddScoped<ITimeRangeService, TimeRangeService>();
+builder.Services.AddScoped<IPositionService, PositionService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
